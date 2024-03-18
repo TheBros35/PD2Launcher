43631a1180798dd685308b6f6a89946700b170bc
Jason P <47406463+PritchardJasonR@users.noreply.github.com>
2024-03-17 21:55:15 -0600

    Text Files in

---
+++ PD2Launcherv2/Views/OptionsView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PD2Launcherv2.Views
+{
+    /// <summary>
+    /// Interaction logic for OptionsView.xaml
+    /// </summary>
+    public partial class OptionsView : Page
+    {
+        public OptionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Assuming you're using a NavigationService in a single window app
+            NavigationService?.GoBack();
+            // If you're using multiple windows, you may need:
+            // this.Close(); or ((Window)this.Parent).Close();
+        }
+    }
+}
